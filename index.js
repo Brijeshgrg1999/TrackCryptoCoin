93c669a8ced4faaa0cde3127ca73425a9431cf44
Gurung brijes <118506279+Brijeshgrg1999@users.noreply.github.com>
2024-03-09 11:35:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ axios.get('https://api.dexscreener.com/latest/dex/tokens/0x6aa3ecec75ceb388d2e92
             }));
         }
 
-        if (cnfrg > 0.00092) {
+        if (cnfrg > 0.0008) {
             messages.push(client.messages.create({
                 body: 'sell cnfrg , ASAP , its over 2 million MC  ',
                 from: '+16592183969',
